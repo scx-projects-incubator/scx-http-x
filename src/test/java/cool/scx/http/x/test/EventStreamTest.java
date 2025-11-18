d0d9f3c58ed113e2c3c583194bd520bacd6f1563
scx567888 <scx567888@outlook.com>
2025-11-18 17:49:50 +0800

    dev

--- src/test/java/cool/scx/http/x/test/EventStreamTest.java
+++ src/test/java/cool/scx/http/x/test/EventStreamTest.java
@@ -5,6 +5,7 @@ import cool.scx.http.media.event_stream.SseEvent;
 import cool.scx.http.media.event_stream.event.EventClientEventStream;
 import cool.scx.http.x.HttpClient;
 import cool.scx.http.x.HttpServer;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 
@@ -12,9 +13,9 @@ public class EventStreamTest {
 
     public static void main(String[] args) throws IOException {
         test1();
-        test2();
     }
 
+    @Test
     public static void test1() throws IOException {
         var httpServer = new HttpServer();
         httpServer.onRequest(c -> {
@@ -22,23 +23,22 @@ public class EventStreamTest {
             c.response().setHeader("Access-Control-Allow-Origin", "*");
             var eventStream = c.response().sendEventStream();
             try (eventStream) {
-                for (int i = 0; i < 20; i = i + 1) {
+                for (int i = 0; i < 100; i = i + 1) {
                     eventStream.send(SseEvent.of("hello" + i).id("123").event("message").comment("这是注释"));
-                    $.sleep(100);
+                    $.sleep(20);
                 }
             }
             System.out.println("全部发送完成");
+            httpServer.stop();
         });
         httpServer.start(8080);
 
-    }
-
-    public static void test2() {
         var client = new HttpClient();
         var eventStream = client.request().uri("http://127.0.0.1:8080").send().body().asEventStream();
         EventClientEventStream.of(eventStream).onEvent(event -> {
             System.err.println(event.event() + " " + event.data());
         }).start();
+
     }
 
 }
