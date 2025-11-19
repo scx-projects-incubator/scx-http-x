1a8a73c470c01d2b3e7f58475cf7ff83a9bed2f1
scx567888 <scx567888@outlook.com>
2025-11-19 17:10:46 +0800

    dev

--- src/main/java/cool/scx/http/x/HttpClientRequest.java
+++ src/main/java/cool/scx/http/x/HttpClientRequest.java
@@ -7,6 +7,8 @@ import cool.scx.http.headers.ScxHttpHeadersWritable;
 import cool.scx.http.media.MediaWriter;
 import cool.scx.http.method.ScxHttpMethod;
 import cool.scx.http.sender.HttpSendException;
+import cool.scx.http.sender.IllegalSenderStateException;
+import cool.scx.http.sender.ScxHttpSenderStatus;
 import cool.scx.http.uri.ScxURI;
 import cool.scx.http.uri.ScxURIWritable;
 import cool.scx.http.version.HttpVersion;
@@ -18,8 +20,10 @@ import cool.scx.http.x.http2.Http2ClientRequest;
 import cool.scx.tcp.ScxTCPSocket;
 
 import java.io.IOException;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static cool.scx.http.method.HttpMethod.GET;
+import static cool.scx.http.sender.ScxHttpSenderStatus.NOT_SENT;
 import static cool.scx.http.version.HttpVersion.HTTP_1_1;
 import static cool.scx.http.version.HttpVersion.HTTP_2;
 import static cool.scx.http.x.http1.request_line.RequestTargetForm.ABSOLUTE_FORM;
@@ -33,25 +37,33 @@ public class HttpClientRequest implements Http1ClientRequest, Http2ClientRequest
 
     private final HttpClient httpClient;
     private final HttpClientOptions options;
+    private final ReentrantLock sendLock;
 
-    protected HttpVersion version;
+    protected HttpVersion[] httpVersions;
     protected ScxHttpMethod method;
     protected ScxURIWritable uri;
     protected ScxHttpHeadersWritable headers;
     protected RequestTargetForm requestTargetForm;
+    public ScxHttpSenderStatus senderStatus;
 
-    public HttpClientRequest(HttpClient httpClient) {
+    public HttpClientRequest(HttpClient httpClient, HttpVersion... httpVersions) {
         this.httpClient = httpClient;
         this.options = httpClient.options();
-        this.version = null;// null 表示自动协商
+        this.sendLock = new ReentrantLock();
+        this.httpVersions = httpVersions; // 空列表 表示自动协商 暂时没用到 因为现在只支持 http1.1
         this.method = GET;
         this.uri = ScxURI.of();
         this.headers = ScxHttpHeaders.of();
         this.requestTargetForm = ORIGIN_FORM;
+        this.senderStatus = ScxHttpSenderStatus.NOT_SENT;
     }
 
-    @Override
-    public ScxHttpClientResponse send(MediaWriter mediaWriter) throws HttpSendException {
+    private ScxHttpClientResponse send0(MediaWriter mediaWriter) throws HttpSendException {
+
+        // 检查发送状态
+        if (senderStatus != NOT_SENT) {
+            throw new IllegalSenderStateException(senderStatus);
+        }
 
         ScxTCPSocket tcpSocket;
 
@@ -84,18 +96,27 @@ public class HttpClientRequest implements Http1ClientRequest, Http2ClientRequest
 
     }
 
+    @Override
+    public ScxHttpClientResponse send(MediaWriter mediaWriter) throws IllegalSenderStateException, HttpSendException {
+        sendLock.lock();
+        try {
+            return send0(mediaWriter);
+        } finally {
+            sendLock.unlock();
+        }
+    }
+
+    @Override
+    public ScxHttpSenderStatus senderStatus() {
+        return senderStatus;
+    }
+
     private String[] getApplicationProtocols() {
         if (this.options.enableHttp2()) {
             return new String[]{HTTP_1_1.alpnValue(), HTTP_2.alpnValue()};
         } else {
             return new String[]{HTTP_1_1.alpnValue()};
         }
-    }
-
-
-    @Override
-    public HttpVersion version() {
-        return version;
     }
 
     @Override
@@ -111,12 +132,6 @@ public class HttpClientRequest implements Http1ClientRequest, Http2ClientRequest
     @Override
     public ScxHttpHeadersWritable headers() {
         return headers;
-    }
-
-    @Override
-    public ScxHttpClientRequest version(HttpVersion version) {
-        this.version = version;
-        return this;
     }
 
     @Override
