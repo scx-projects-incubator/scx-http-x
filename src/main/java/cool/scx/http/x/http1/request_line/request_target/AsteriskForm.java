fcb6813947ddcd7520be97fe66b8629d1d4e37a5
scx567888 <scx567888@outlook.com>
2025-12-22 18:49:41 +0800

    dev

--- src/main/java/cool/scx/http/x/http1/request_line/request_target/AsteriskForm.java
+++ src/main/java/cool/scx/http/x/http1/request_line/request_target/AsteriskForm.java
@@ -3,6 +3,12 @@ package cool.scx.http.x.http1.request_line.request_target;
 /// 例: `OPTIONS * HTTP/1.1`
 ///
 /// 仅与 `OPTIONS` 方法一起使用, 针对整个服务器
-public class AsteriskForm implements RequestTarget {
+public final class AsteriskForm implements RequestTarget {
+
+    public static final AsteriskForm ASTERISK_FORM = new AsteriskForm();
+
+    private AsteriskForm() {
+
+    }
 
 }
