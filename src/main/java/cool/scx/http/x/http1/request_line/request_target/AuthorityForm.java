c8eedaef019a70ef9dc4149327556975099f74e8
scx567888 <scx567888@outlook.com>
2025-12-22 18:45:22 +0800

    dev

--- src/main/java/cool/scx/http/x/http1/request_line/request_target/AuthorityForm.java
+++ src/main/java/cool/scx/http/x/http1/request_line/request_target/AuthorityForm.java
@@ -1,8 +1,48 @@
 package cool.scx.http.x.http1.request_line.request_target;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /// 例: `CONNECT example.com:443 HTTP/1.1`
 ///
 /// 仅与 `CONNECT` 方法一起使用
-public class AuthorityForm implements RequestTarget{
+public record AuthorityForm(String host, int port) implements RequestTarget {
+
+    public static AuthorityForm of(String authority) throws URISyntaxException {
+        var u = new URI("scx://" + authority);
+
+        var host = u.getHost();
+        var port = u.getPort();
+        var rawPath = u.getRawPath();
+        var rawQuery = u.getRawQuery();
+        var rawFragment = u.getRawFragment();
+        var rawUserInfo = u.getRawUserInfo();
+
+        if (host == null || host.isEmpty()) {
+            throw new URISyntaxException(authority, "Invalid authority: host is missing");
+        }
+
+        if (port <= 0 || port > 65535) {
+            throw new URISyntaxException(authority, "Invalid authority: port is missing or invalid");
+        }
+
+        if (!"".equals(rawPath)) {
+            throw new URISyntaxException(authority, "Invalid authority: path not allowed");
+        }
+
+        if (rawQuery != null) {
+            throw new URISyntaxException(authority, "Invalid authority: query not allowed");
+        }
+
+        if (rawFragment != null) {
+            throw new URISyntaxException(authority, "Invalid authority: fragment not allowed");
+        }
+
+        if (rawUserInfo != null) {
+            throw new URISyntaxException(authority, "Invalid authority: user-info not allowed");
+        }
+
+        return new AuthorityForm(u.getHost(), u.getPort());
+    }
 
 }
