9dd2f8aa2d6cecd9c5d77d16ee0d607356e0a389
scx567888 <scx567888@outlook.com>
2025-12-23 13:42:50 +0800

    dev

--- src/main/java/cool/scx/http/x/http1/request_line/request_target/RequestTarget.java
+++ src/main/java/cool/scx/http/x/http1/request_line/request_target/RequestTarget.java
@@ -2,7 +2,7 @@ package cool.scx.http.x.http1.request_line.request_target;
 
 import dev.scx.http.uri.ScxURI;
 
-public interface RequestTarget {
+public sealed interface RequestTarget permits AbsoluteForm, AsteriskForm, AuthorityForm, OriginForm {
 
     ScxURI toScxURI();
 
