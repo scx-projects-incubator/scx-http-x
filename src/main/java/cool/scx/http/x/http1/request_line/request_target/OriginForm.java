32bd83e0d8c3b0fa4ba83c2e9ff975395e8d041d
scx567888 <scx567888@outlook.com>
2025-12-23 10:52:05 +0800

    dev

--- src/main/java/cool/scx/http/x/http1/request_line/request_target/OriginForm.java
+++ src/main/java/cool/scx/http/x/http1/request_line/request_target/OriginForm.java
@@ -8,6 +8,7 @@ import java.net.URISyntaxException;
 /// 用于大多数客户端请求
 public record OriginForm(String path, String query, String fragment) implements RequestTarget {
 
+    /// 这里 origin 必定是 "/" 起始, 如果不是 那么结果是不可靠的.
     public static OriginForm of(String origin) throws URISyntaxException {
         // 我们借用 URI 来作为 解析器
         var u = new URI(origin);
