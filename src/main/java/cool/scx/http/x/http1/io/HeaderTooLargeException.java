bb87583fd81f41a25f2ae948a025d2ca4abde859
scx567888 <scx567888@outlook.com>
2025-12-24 12:16:33 +0800

    dev

---
+++ src/main/java/cool/scx/http/x/http1/io/HeaderTooLargeException.java
@@ -0,0 +1,18 @@
+package cool.scx.http.x.http1.io;
+
+import dev.scx.http.exception.ScxHttpException;
+import dev.scx.http.status_code.HttpStatusCode;
+import dev.scx.http.status_code.ScxHttpStatusCode;
+
+public final class HeaderTooLargeException extends Exception implements ScxHttpException {
+
+    public HeaderTooLargeException(String message) {
+        super(message);
+    }
+
+    @Override
+    public ScxHttpStatusCode statusCode() {
+        return HttpStatusCode.REQUEST_HEADER_FIELDS_TOO_LARGE;
+    }
+
+}
---
+++ src/main/java/cool/scx/http/x/http1/io/StatusLineToLongException.java
@@ -0,0 +1,9 @@
+package cool.scx.http.x.http1.io;
+
+public final class StatusLineToLongException extends Exception {
+
+    public StatusLineToLongException(String message) {
+        super(message);
+    }
+
+}
