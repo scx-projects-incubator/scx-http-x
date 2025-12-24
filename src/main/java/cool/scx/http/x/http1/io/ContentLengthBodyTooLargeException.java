7b85a283bd1dd315a433cc8eea76c3cc06c057e6
scx567888 <scx567888@outlook.com>
2025-12-24 13:08:19 +0800

    dev

--- src/main/java/cool/scx/http/x/http1/io/ContentLengthBodyTooLargeException.java
+++ src/main/java/cool/scx/http/x/http1/io/ContentLengthBodyTooLargeException.java
@@ -2,7 +2,6 @@ package cool.scx.http.x.http1.io;
 
 import dev.scx.http.exception.ScxHttpException;
 import dev.scx.http.status_code.ScxHttpStatusCode;
-import dev.scx.io.exception.ScxIOException;
 
 import static dev.scx.http.status_code.HttpStatusCode.CONTENT_TOO_LARGE;
 
