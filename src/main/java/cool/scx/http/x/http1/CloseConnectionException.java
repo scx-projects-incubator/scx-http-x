c2f291674f232eea57f8ea310a11f8f1e93afcc2
scx567888 <scx567888@outlook.com>
2025-12-24 13:56:19 +0800

    dev

--- src/main/java/cool/scx/http/x/http1/CloseConnectionException.java
+++ src/main/java/cool/scx/http/x/http1/CloseConnectionException.java
@@ -3,7 +3,7 @@ package cool.scx.http.x.http1;
 /// 主动关闭 Socket异常  (仅内部使用)
 /// 调用方 当捕获到此异常的时候 应该直接断开连接
 /// 被调用方 想中断 socket 连接 请抛出此异常
-public final class CloseConnectionException extends RuntimeException {
+final class CloseConnectionException extends Exception {
 
     /// 只允许内部创建
     CloseConnectionException() {
